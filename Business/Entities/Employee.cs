69e4add60685db37d2be8c21750204637149108b
LeDuy17122000 <58205622+LeDuy17122000@users.noreply.github.com>
2026-03-29 22:26:36 +0700

    2

---
+++ Business/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Business.Entities
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
