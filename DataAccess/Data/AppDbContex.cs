8f529481c415a8a43ec72d0901a16df984083f2b
LeDuy17122000 <58205622+LeDuy17122000@users.noreply.github.com>
2026-04-01 12:29:04 +0700

    4

--- DataAccess/Data/AppDbContex.cs
+++ DataAccess/Data/AppDbContex.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Business.Entities;
 
 namespace DataAccess.Data
 {
-    internal class AppDbContex
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> employees { get; set; }
     }
 }
