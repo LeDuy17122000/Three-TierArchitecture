b47610f4105527e66fd01d3ce99d499953555ec2
LeDuy17122000 <58205622+LeDuy17122000@users.noreply.github.com>
2026-04-04 22:54:05 +0700

    7

---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using DataAccess.Repositories;
+using Business.Entities;
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployee employee;
+        public EmployeeController(IEmployee employeeRepo)
+        {
+            this.employee = employeeRepo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var data = await employee.GetAsync();
+            return Ok(data);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await employee.GetByIdAsync(id);
+            return Ok(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] Employee employeeDto)
+        {
+            var data = await this.employee.AddAsync(employeeDto);
+            return Ok(data);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Employee employeeDto)
+        {
+            var result = await employee.UpdateAsync(employeeDto);
+            return Ok(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await employee.DeleteAsync(id);
+            return Ok(result);
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using DataAccess.Data;
+using DataAccess.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IEmployee, EmployeeRepo>();  
 var app = builder.Build();
 
 // Middleware
